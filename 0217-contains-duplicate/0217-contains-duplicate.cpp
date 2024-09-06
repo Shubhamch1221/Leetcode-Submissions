0ff92d8bdd96f6bcfe0e3e203003d64dd7412ae5
Shubhamch1221 <137816380+Shubhamch1221@users.noreply.github.com>
2024-09-06 23:18:47 +0530

    Time: 104 ms (30.75%), Space: 75.6 MB (17.73%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n =nums.size();
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1])return true;
-        }
-        return false;
-    }
-};
+//         // t=o[nlogn],o[1]
+//         sort(nums.begin(),nums.end());
+//         int n =nums.size();
+//         for(int i=0;i<n-1;i++){
+//             if(nums[i]==nums[i+1])return true;
+//         }
+//         return false;
+//using set
+    unordered_set<int>s;
+    int n=nums.size();
+    for(int i=0;i<n;i++)s.insert(nums[i]);
+    if(nums.size()==s.size())return false;
+    return true;
+     }
+ };
