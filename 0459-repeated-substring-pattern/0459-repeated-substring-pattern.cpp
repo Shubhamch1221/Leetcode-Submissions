ccf12c0486852a157315183b6b739c5cb498c19a
Shubhamch1221 <137816380+Shubhamch1221@users.noreply.github.com>
2024-12-12 22:03:42 +0530

    Time: 59 ms (27.48%), Space: 94.7 MB (14.98%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         int n = s.length();  // Step 1: Get the length of the string
-
-        // Step 2: Try every possible length for the repeated substring
+         // Step 2: Try every possible length for the repeated substring
         for (int i = 1; i <= n / 2; i++) {
             if (n % i == 0) {  // Step 3: Check if the length divides the total length of the string
                 string subs = s.substr(0, i);  // Step 4: Get the substring of length i from the start
                 string repeat = "";  // Step 5: Prepare an empty string to repeat the substring
-
-                // Step 6: Repeat the substring enough times to match the full length of the original string
+             // Step 6: Repeat the substring enough times to match the full length of the original string
                 for (int j = 0; j < n / i; j++) {
                     repeat += subs;
                 }
